fb988e7c03c9acc66c28d184cc436be999f0586e
Kenji Okimoto <okimoto@clear-code.com>
2017-07-21 10:05:40 +0900

    Implement SSL handshake

--- influent-transport/src/main/java/influent/internal/nio/NioSslAcceptor.java
+++ influent-transport/src/main/java/influent/internal/nio/NioSslAcceptor.java
@@ -4,24 +4,23 @@ import influent.exception.InfluentIOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLEngineResult;
-import javax.net.ssl.TrustManager;
+import javax.net.ssl.SSLException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
 import java.nio.channels.AlreadyBoundException;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.UnsupportedAddressTypeException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 
@@ -31,6 +30,8 @@ public class NioSslAcceptor implements NioAttachment {
   private final Consumer<SocketChannel> callback;
   private final ServerSocketChannel serverSocketChannel;
   private final SSLContext context;
+
+  private ExecutorService executor = Executors.newSingleThreadExecutor();
 
   /**
    * @param localAddress
@@ -113,23 +114,77 @@ public class NioSslAcceptor implements NioAttachment {
         logger.debug("Connection closed due to handshake failure.");
       }
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new InfluentIOException(e.getMessage(), e);
     }
     return null;
   }
 
-  private boolean doHandshake(SocketChannel socketChannel, SSLEngine engine) {
+  private boolean doHandshake(SocketChannel socketChannel, SSLEngine engine) throws IOException {
     logger.debug("Handshaking...");
     SSLEngineResult result;
     SSLEngineResult.HandshakeStatus status;
+    int bufferSize = engine.getSession().getApplicationBufferSize();
+    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+    ByteBuffer unwrappedBuffer = ByteBuffer.allocate(bufferSize);
+    ByteBuffer wrappedBuffer = ByteBuffer.allocate(bufferSize);
 
     status = engine.getHandshakeStatus();
     while (status != SSLEngineResult.HandshakeStatus.FINISHED &&
         status != SSLEngineResult.HandshakeStatus.NOT_HANDSHAKING) {
       switch (status) {
         case NEED_UNWRAP:
+          if (socketChannel.read(buffer) < 0) {
+            if (engine.isInboundDone() && engine.isOutboundDone()) {
+              return false;
+            }
+            try {
+              engine.closeInbound();
+            } catch (SSLException e) {
+              logger.error(e.getMessage());
+            }
+            engine.closeOutbound();
+            status = engine.getHandshakeStatus();
+            break;
+          }
+          buffer.flip();
+          result = engine.unwrap(buffer, unwrappedBuffer);
+          buffer.compact();
+          status = engine.getHandshakeStatus();
+          switch (result.getStatus()) {
+            case OK:
+              break;
+            case BUFFER_OVERFLOW:
+              unwrappedBuffer = ByteBuffer.allocate(unwrappedBuffer.capacity() * 2);
+              break;
+            case BUFFER_UNDERFLOW:
+              // TODO
+              break;
+            case CLOSED:
+              break;
+            default:
+              throw new IllegalStateException("Invalid SSL status: " + result.getStatus());
+          }
           break;
         case NEED_WRAP:
+          wrappedBuffer.clear();
+          result = engine.wrap(buffer, wrappedBuffer);
+          status = result.getHandshakeStatus();
+          switch (result.getStatus()) {
+            case OK:
+              wrappedBuffer.flip();
+              while (wrappedBuffer.hasRemaining()) {
+                socketChannel.write(wrappedBuffer);
+              }
+              break;
+            case BUFFER_OVERFLOW:
+              wrappedBuffer = ByteBuffer.allocate(wrappedBuffer.capacity() * 2);
+              break;
+            case BUFFER_UNDERFLOW:
+              throw new SSLException("Buffer underflow after a wrap. Must not happen.");
+            case CLOSED:
+            default:
+              throw new IllegalStateException("Invalid SSL status: " + result.getStatus());
+          }
           break;
         case NEED_TASK:
           Runnable task;
@@ -138,6 +193,8 @@ public class NioSslAcceptor implements NioAttachment {
           }
           break;
         case FINISHED:
+          break;
+        case NOT_HANDSHAKING:
           break;
         default:
           throw new IllegalStateException("Invalid SSL status: " + status);
