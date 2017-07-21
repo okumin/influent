86f7fde5ae7d801b777bbf3a6d519fcec196c657
Kenji Okimoto <okimoto@clear-code.com>
2017-07-21 15:01:58 +0900

    Implement createKeyManagers and createTrustManagers

--- influent-java/src/main/java/influent/forward/NioForwardServer.java
+++ influent-java/src/main/java/influent/forward/NioForwardServer.java
@@ -16,11 +16,23 @@
 
 package influent.forward;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
+import java.security.PrivateKey;
 import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.function.Consumer;
@@ -31,8 +43,12 @@ import influent.internal.nio.NioSslAcceptor;
 import influent.internal.nio.NioTcpAcceptor;
 
 import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509KeyManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * A {@code ForwardServer} implemented by NIO.
@@ -146,12 +162,46 @@ final class NioForwardServer implements ForwardServer {
   }
 
   private KeyManager[] createKeyManagers(String filepath, String keystorePassword, String keyPassword) {
-    // TODO
+    try {
+      KeyStore keyStore = KeyStore.getInstance("JKS");
+      InputStream keyStoreIS = new FileInputStream(filepath);
+      try {
+        keyStore.load(keyStoreIS, keystorePassword.toCharArray());
+      } finally {
+        if (keyStoreIS != null) {
+          keyStoreIS.close();
+        }
+      }
+      KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      kmf.init(keyStore, keyPassword.toCharArray());
+      return kmf.getKeyManagers();
+    } catch (final IOException e) {
+      e.printStackTrace();
+    } catch (final CertificateException | UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException e) {
+      e.printStackTrace();
+    }
     return null;
   }
 
   private TrustManager[] createTrustManagers(String filepath, String keystorePassword) {
-    // TODO
+    try {
+      KeyStore trustStore = KeyStore.getInstance("JKS");
+      InputStream trustStoreIS = new FileInputStream(filepath);
+      try {
+        trustStore.load(trustStoreIS, keystorePassword.toCharArray());
+      } finally {
+        if (trustStoreIS != null) {
+          trustStoreIS.close();
+        }
+      }
+      TrustManagerFactory trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      trustFactory.init(trustStore);
+      return trustFactory.getTrustManagers();
+    } catch (final IOException e) {
+      e.printStackTrace();
+    } catch (final CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
     return null;
   }
 
