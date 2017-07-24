a71e988ac405942fb36b5513207963aa360b5c5b
Kenji Okimoto <okimoto@clear-code.com>
2017-07-24 12:09:24 +0900

    Use NioChannelConfig

--- influent-java/src/main/java/influent/forward/NioForwardServer.java
+++ influent-java/src/main/java/influent/forward/NioForwardServer.java
@@ -16,39 +16,18 @@
 
 package influent.forward;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Principal;
-import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.function.Consumer;
 
 import influent.internal.nio.NioEventLoop;
 import influent.internal.nio.NioEventLoopPool;
-import influent.internal.nio.NioSslAcceptor;
+import influent.internal.nio.NioChannelConfig;
 import influent.internal.nio.NioTcpAcceptor;
 
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509KeyManager;
-import javax.net.ssl.X509TrustManager;
 
 /**
  * A {@code ForwardServer} implemented by NIO.
@@ -56,8 +35,7 @@ import javax.net.ssl.X509TrustManager;
 final class NioForwardServer implements ForwardServer {
   private final NioEventLoop bossEventLoop;
   private final NioEventLoopPool workerEventLoopPool;
-  private final Protocol protocol;
-  private final TlsVersion tlsVersion;
+  private final NioChannelConfig sslConfig;
   private SSLContext context = null;
 
   /**
@@ -87,7 +65,7 @@ final class NioForwardServer implements ForwardServer {
                    final int workerPoolSize) {
     this(localAddress, callback, chunkSizeLimit, backlog,
         sendBufferSize, receiveBufferSize, keepAliveEnabled, tcpNoDelayEnabled,
-        workerPoolSize, Protocol.TCP, TlsVersion.None);
+        workerPoolSize, new NioChannelConfig());
     context = null;
   }
 
@@ -100,92 +78,18 @@ final class NioForwardServer implements ForwardServer {
                    final boolean keepAliveEnabled,
                    final boolean tcpNoDelayEnabled,
                    final int workerPoolSize,
-                   final Protocol protocol,
-                   final TlsVersion tlsVersion) {
+                   final NioChannelConfig sslConfig) {
     bossEventLoop = NioEventLoop.open();
     workerEventLoopPool = NioEventLoopPool.open(workerPoolSize);
-    this.protocol = protocol;
-    this.tlsVersion = tlsVersion;
-    switch (protocol) {
-      case TCP:
-        context = null;
-        final Consumer<SocketChannel> tcpChannelFactory = socketChannel -> new NioForwardConnection(
-            socketChannel, workerEventLoopPool.next(), callback, chunkSizeLimit, sendBufferSize,
-            keepAliveEnabled, tcpNoDelayEnabled
-        );
-        new NioTcpAcceptor(
-            localAddress, bossEventLoop, tcpChannelFactory, backlog, receiveBufferSize
-        );
-        break;
-      case TLS:
-        try {
-          context = SSLContext.getInstance(tlsVersion.toString());
-          context.init(
-              createKeyManagers("", "", ""),
-              createTrustManagers("", ""),
-              new SecureRandom()
-          );
-        } catch (NoSuchAlgorithmException e) {
-          throw new AssertionError(e.getMessage());
-        } catch (KeyManagementException e) {
-          e.printStackTrace();
-        }
-        final Consumer<SocketChannel> tlsChannelFactory = socketChannel -> new NioSslForwardConnection(
-            socketChannel, workerEventLoopPool.next(), callback, chunkSizeLimit, sendBufferSize,
-            keepAliveEnabled, tcpNoDelayEnabled, context
-        );
-        new NioSslAcceptor(
-            localAddress, bossEventLoop, tlsChannelFactory, backlog, receiveBufferSize, context
-        );
-        break;
-      default:
-        throw new IllegalArgumentException("Unknow protocol: " + protocol.name());
-    }
+    this.sslConfig = sslConfig;
+    final Consumer<SocketChannel> tcpChannelFactory = socketChannel -> new NioForwardConnection(
+        socketChannel, workerEventLoopPool.next(), callback, chunkSizeLimit, sendBufferSize,
+        keepAliveEnabled, tcpNoDelayEnabled, sslConfig
+    );
+    new NioTcpAcceptor(
+        localAddress, bossEventLoop, tcpChannelFactory, backlog, receiveBufferSize
+    );
     new NioUdpHeartbeatServer(localAddress, bossEventLoop);
-  }
-
-  private KeyManager[] createKeyManagers(String filepath, String keystorePassword, String keyPassword) {
-    try {
-      KeyStore keyStore = KeyStore.getInstance("JKS");
-      InputStream keyStoreIS = new FileInputStream(filepath);
-      try {
-        keyStore.load(keyStoreIS, keystorePassword.toCharArray());
-      } finally {
-        if (keyStoreIS != null) {
-          keyStoreIS.close();
-        }
-      }
-      KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-      kmf.init(keyStore, keyPassword.toCharArray());
-      return kmf.getKeyManagers();
-    } catch (final IOException e) {
-      e.printStackTrace();
-    } catch (final CertificateException | UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException e) {
-      e.printStackTrace();
-    }
-    return null;
-  }
-
-  private TrustManager[] createTrustManagers(String filepath, String keystorePassword) {
-    try {
-      KeyStore trustStore = KeyStore.getInstance("JKS");
-      InputStream trustStoreIS = new FileInputStream(filepath);
-      try {
-        trustStore.load(trustStoreIS, keystorePassword.toCharArray());
-      } finally {
-        if (trustStoreIS != null) {
-          trustStoreIS.close();
-        }
-      }
-      TrustManagerFactory trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-      trustFactory.init(trustStore);
-      return trustFactory.getTrustManagers();
-    } catch (final IOException e) {
-      e.printStackTrace();
-    } catch (final CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
-      e.printStackTrace();
-    }
-    return null;
   }
 
   /**
